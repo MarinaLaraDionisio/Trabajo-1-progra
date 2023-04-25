edd95ca4fd40fc5d5d50d8df056990a97eab2ab3
MarinaLaraDionisio <131800265+MarinaLaraDionisio@users.noreply.github.com>
2023-04-25 19:09:38 +0200

    Create recursivo.c

---
+++ recursivo.c
@@ -0,0 +1,27 @@
+float potenciaRecursiva(float numero, float potencia)
+{
+    if (potencia <= 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return numero * potenciaRecursiva(numero, potencia - 1);
+    }
+}
+
+int main(){
+  float x; int p;
+  
+  printf("Introduzca el valor del número que quiere elevar: ");
+  scanf("%f", &x);
+  
+  printf("Introduzca el valor de la potenncia a la que quiere elevar: ");
+  scanf("%d", &p);
+  
+  x=potenciaRecursiva(x, p);
+  
+  printf("El valor obtenido es: %f", x)
+  
+  return 0;
+}
