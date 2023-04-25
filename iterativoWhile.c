4e407cfcc969ed998de47e51e7ffd2f37ede9585
MarinaLaraDionisio <131800265+MarinaLaraDionisio@users.noreply.github.com>
2023-04-25 19:12:17 +0200

    Create iterativoWhile.c

---
+++ iterativoWhile.c
@@ -0,0 +1,27 @@
+float potenciaWhile(float numero, int potencia)
+{
+    long resultado = numero;
+    while (potencia > 1)
+    {
+        resultado = resultado * numero;
+        potencia--;
+    }
+    return resultado;
+}
+
+int main(){
+  float x; int p;
+  
+  printf("Introduzca el valor del número que quiere elevar: ");
+  scanf("%f", &x);
+  
+  printf("Introduzca el valor de la potenncia a la que quiere elevar: ");
+  scanf("%d", &p);
+  
+  x=potenciaWhile(x, p);
+  
+  printf("El valor obtenido es: %f", x)
+  
+  return 0;
+}
+}
