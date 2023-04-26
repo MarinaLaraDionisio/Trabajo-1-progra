f7149e39e5031b64f6873b3b650106daa8f51a2d
Guzmanelbueno <131880631+Guzmanelbueno@users.noreply.github.com>
2023-04-26 20:15:56 +0200

    Update experimentor.c

--- experimentor.c
+++ experimentor.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+float x; int p;
+int main(){
+  int citer, crecur;
+  
+  printf("Introduzca el valor del número que quiere elevar: ");
+  scanf("%f", &x);
+  
+  printf("Introduzca el valor de la potenncia a la que quiere elevar: ");
+  scanf("%d", &p);
+  
+  x=potenciaRecursiva(x, p);
+ 
+  printf("El valor obtenido mediante el método iterativo es: %f", x);
+  
+  return 0;
+}
