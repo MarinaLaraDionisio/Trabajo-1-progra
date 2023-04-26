a16d61e8493dcb808dd06a7b631404a901278617
Guzmanelbueno <131880631+Guzmanelbueno@users.noreply.github.com>
2023-04-26 20:18:38 +0200

    Update experimentoi.c

--- experimentoi.c
+++ experimentoi.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
+#include<time.h>
 float x; int p;
 int main(){
   int citer, crecur;
-  
+  double time;
   printf("Introduzca el valor del número que quiere elevar: ");
   scanf("%f", &x);
   
   printf("Introduzca el valor de la potenncia a la que quiere elevar: ");
   scanf("%d", &p);
-  
+  clock_t begin = clock();
   x=potenciaWhile(x, p, &citer);
-  
+  clock_t end = clock();
+  tiempo = (double)(end - begin) / CLOCKS_PER_SEC;
   printf("El valor obtenido mediante el método iterativo es: %f", x);
   printf("Las iteraciones que hemos necesitado son: %d", citer);
   
