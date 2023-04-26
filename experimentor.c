cbe2ca8c55d9252803159c8275a1b39c14f3d69c
Guzmanelbueno <131880631+Guzmanelbueno@users.noreply.github.com>
2023-04-26 20:20:55 +0200

    Update experimentor.c

--- experimentor.c
+++ experimentor.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 float x; int p;
 int main(){
-  int citer, crecur;
+  int crecur;
+  double tiempo;
   
   printf("Introduzca el valor del número que quiere elevar: ");
   scanf("%f", &x);
   
   printf("Introduzca el valor de la potenncia a la que quiere elevar: ");
   scanf("%d", &p);
-  
+  clock_t begin = clock();
   x=potenciaRecursiva(x, p);
- 
+  clock_t end = clock();
+  tiempo= (double)(end - begin) / CLOCKS_PER_SEC;
   printf("El valor obtenido mediante el método iterativo es: %f", x);
   
   return 0;
