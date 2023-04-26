680d2f41c1bec28a5a139d8679978267913797de
Guzmanelbueno <131880631+Guzmanelbueno@users.noreply.github.com>
2023-04-26 18:45:49 +0200

    Update iterativoWhile.c

--- iterativoWhile.c
+++ iterativoWhile.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 #include"funciones.h"
-float potenciaWhile(float numero, int potencia)
+float potenciaWhile(float numero, int potencia, int *citer)
 {
     long resultado = numero;
+    if(potencia==0){
+    return 1;
+    }
     while (potencia > 1)
     {
         resultado = resultado * numero;
         potencia--;
+        *citer++;
     }
     return resultado;
 }
