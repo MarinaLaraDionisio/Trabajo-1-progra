7785d7d60c63698f8d7cfbdbaf5895414204340e
Guzmanelbueno <131880631+Guzmanelbueno@users.noreply.github.com>
2023-04-26 17:58:19 +0200

    Update experimento.c

--- experimento.c
+++ experimento.c
@@ -1,1 +1,15 @@
-
+int main(){
+  float x; int p;
+  
+  printf("Introduzca el valor del número que quiere elevar: ");
+  scanf("%f", &x);
+  
+  printf("Introduzca el valor de la potenncia a la que quiere elevar: ");
+  scanf("%d", &p);
+  
+  x=potenciaWhile(x, p);
+  
+  printf("El valor obtenido es: %f", x)
+  
+  return 0;
+}
