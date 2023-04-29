9c65bd3499363d0379d5634bc68c50b0d6fec275
Guzmanelbueno <131880631+Guzmanelbueno@users.noreply.github.com>
2023-04-29 20:56:22 +0200

    Update experimentoi.c

--- experimentoi.c
+++ experimentoi.c
@@ -1,21 +1,22 @@
-#include<stdio.h>
-#include<time.h>
-float n; int p;
+#include <stdio.h>
+#include <time.h>
+#include "funciones.h"
+float n; int p, citer, crecur;
 int main(){
-  int citer;
-  double time;
+  /*int citer;*/
   printf("Introduzca el valor del número que quiere elevar: ");
   scanf("%f", &n);
-  
+
   printf("Introduzca el valor de la potenncia a la que quiere elevar: ");
   scanf("%d", &p);
-  clock_t begin = clock();
-  x=potenciaWhile(n, p, &citer);
-  clock_t end = clock();
-  tiempo = (double)(end - begin) / CLOCKS_PER_SEC;
-  printf("El valor obtenido mediante el método iterativo es: %f", x);
-  printf("Las iteraciones que hemos necesitado son: %d", citer);
-  printf("El tiempo empleado es %f", tiempo);
-  
+  time_t begin = time(NULL);
+  potenciaWhile(n, p, citer);
+  time_t end = time(NULL);
+  /*printf("El valor obtenido mediante el método iterativo es: %f\n", n);
+  printf("Las iteraciones que hemos necesitado son: %d\n", citer);*/
+  printf("El tiempo empleado en el algoritmo iterativo es %d", (end - begin));
+  n=potenciaRecursiva(n, p, &crecur);
+printf("El valor obtenido mediante el método recursivo es: %f\n", n);
+  printf("Las iteraciones que hemos necesitado son: %d\n", citer);
   return 0;
 }
