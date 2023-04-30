d110afc6817b54aef78653530592783f4cd97503
SantiRomero12 <131960352+SantiRomero12@users.noreply.github.com>
2023-05-01 01:22:43 +0200

    Create recursivoopcion2.c

---
+++ recursivoopcion2.c
@@ -0,0 +1,45 @@
+# Trabajo-1-progra
+Guion de prácticas para algoritmo de calcular potencia, recursivo e iterativo
+
+#include <stdio.h>
+ 
+// Haciendo uso de la propuesta de Alvaro en el guion del trabajo para el recursivo he decidido usar la funcion power como os dije en clase la semana pasada como recordareis, a ver como lo veis
+
+long power(int x, int e)
+{
+    // condición base
+    if (e == 0) {
+        return 1L;
+    }
+ 
+    if (e & 1) { 
+    
+    // para e impar
+    
+        return x * power(x, e / 2) * power(x, e / 2);
+    }
+    
+    else { 
+    
+     // para e par
+    
+    return power(x, e / 2) * power(x, e / 2);
+    
+    }
+ 
+}
+ 
+int main(void)
+{
+    int x;
+    int e;
+    
+    printf("Introduzca el numero cuya potencia quiera calcular \n");
+    scanf("%d", &x);
+    printf("Introduzca el orden de la potencia a calcular del numero anteriormente citado \n");
+    scanf("%d", &e);
+    
+    printf("pow(%d, %d) = %d", x, e, power(x, e));
+ 
+    return 0;
+}
