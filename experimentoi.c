1eb316f1fdcc204cfe9103bb3f9d72b7862e9985
MarinaLaraDionisio <131800265+MarinaLaraDionisio@users.noreply.github.com>
2023-04-30 21:20:53 +0200

    Update experimentoi.c

--- experimentoi.c
+++ experimentoi.c
@@ -8,16 +8,16 @@ int main(){
   printf("Introduzca el valor del número que quiere elevar: ");
   scanf("%lf", &n);
 
-  printf("Introduzca el valor de la potenncia a la que quiere elevar: ");
+  printf("Introduzca el valor de la potencia a la que quiere elevar: ");
   scanf("%d", &p);
   time_t begin = time(NULL);
   potenciaWhile(n, p, citer);
   time_t end = time(NULL);
-  /*printf("El valor obtenido mediante el método iterativo es: %f\n", n);
-  printf("Las iteraciones que hemos necesitado son: %d\n", citer);*/
-  printf("El tiempo empleado en el algoritmo iterativo es %d", (end - begin));
-  n=potenciaRecursiva(n, p, &crecur);
-printf("El valor obtenido mediante el método recursivo es: %f\n", n);
+  printf("El valor obtenido mediante el método iterativo es: %f\n", n);
   printf("Las iteraciones que hemos necesitado son: %d\n", citer);
+  printf("El tiempo empleado en el algoritmo iterativo es %d", (end - begin));
+ /* n=potenciaRecursiva(n, p, &crecur);
+printf("El valor obtenido mediante el método recursivo es: %f\n", n);
+  printf("Las iteraciones que hemos necesitado son: %d\n", citer);*/
   return 0;
 }
