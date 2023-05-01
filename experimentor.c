f50703ef2b0d788b6cee35b20ca7c99a20d3890c
MarinaLaraDionisio <marina.lara@alumnos.upm.es>
2023-05-01 17:51:05 +0200

    Update experimentor.c

--- experimentor.c
+++ experimentor.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
 #include <time.h>
-#include <stdlib.h>
 #include "funciones.h"
 float n; int p;
 int main(){
   int crecur;
-  double tiempo;
+  clock_t t; double tiempo;
 
   printf("Introduzca el valor que quiere elevar: ");
   scanf("%f", &n);
 
   printf("Introduzca el valor de la potencia a la que quiere elevar: ");
   scanf("%d", &p);
-  time_t begin = time(NULL);
+  t=clock();
+  n=potenciaRecursiva(n, p, &crecur);
+  t = clock()-t;
+
+  tiempo= ((double)t)/CLOCKS_PER_SEC;
+
+ /* time_t begin = time(NULL);
   n=potenciaRecursiva(n, p, &crecur);
   time_t end = time(NULL);
-  tiempo= (double)(end - begin);
-  printf("El valor obtenido mediante recursividad es: %f", n);
+  tiempo= (double)((end - begin);*/
+
+  printf("El valor obtenido mediante recursividad es: %f\n", n);
+
+  printf("El tiempo empleado en ejecutar el archivo es: %f", tiempo);
+
+  char *nombreArchivo = "recursivo.txt";
+    char *modo = "a+";// w es para sobrescribir, a+ es para añadir al existente
+    FILE *archivo = fopen(nombreArchivo, modo);
+    if (archivo == NULL) {
+        printf("Error abriendo archivo %s", nombreArchivo);
+        return 1;
+    }
+    fprintf(archivo, "%f \n", tiempo);
+    fclose(archivo);
+    puts("\nNuevo dato guardado");
 
   return 0;
 }
+
 
