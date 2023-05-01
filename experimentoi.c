f939f4c92e33651e2ae603d8fe3493bd2a6ba4ea
MarinaLaraDionisio <marina.lara@alumnos.upm.es>
2023-05-01 18:01:08 +0200

    Update experimentoi.c

--- experimentoi.c
+++ experimentoi.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
-#include <sys/time.h>
+#include <time.h>
 #include "funciones.h"
-float num; int pot, citer, crecur;
+float n; int p;
 int main(){
-  /*int citer;*/
-  struct timeval ini, fin;
-  double tiempo;
-  printf("Introduzca el valor del numero que quiere elevar: ");
-  scanf("%f", &num);
+ int crecur;
+  clock_t t; double tiempo;
 
-  printf("Introduzca el valor de la potenncia a la que quiere elevar: ");
-  scanf("%d", &pot);
-  getchar();
-  gettimeofday(&ini, NULL); //Inicio de la ejecución
-  potenciaWhile(num, pot, citer);
-  gettimeofday(&fin, NULL);
-  tiempo=((fin.tv_sec-ini.tv_sec)*1000+(fin.tv_usec-ini.tv_usec)/1000.0)/1000;
-  printf("El tiempo empleado en el algoritmo iterativo es %lf", tiempo);
-  /*num=potenciaRecursiva(num, pot, &crecur);
-  printf("El valor obtenido mediante el método recursivo es: %f\n", num);
-  printf("Las iteraciones que hemos necesitado son: %d\n", crecur);*/
+  printf("Introduzca el valor que quiere elevar: ");
+  scanf("%f", &n);
+
+  printf("Introduzca el valor de la potencia a la que quiere elevar: ");
+  scanf("%d", &p);
+  t=clock();
+  n=potenciaWhile(n, p, 0);
+  t = clock()-t;
+
+  tiempo= ((double)t)/CLOCKS_PER_SEC;
+
+ /* time_t begin = time(NULL);
+  n=potenciaRecursiva(n, p, &crecur);
+  time_t end = time(NULL);
+  tiempo= (double)((end - begin);*/
+
+  printf("El valor obtenido mediante recursividad es: %f\n", n);
+
+  printf("El tiempo empleado en ejecutar el archivo es: %f", tiempo);
+
+  char *nombreArchivo = "recursivo.txt";
+    char *modo = "a+";// w es para sobrescribir, a+ es para añadir al existente
+    FILE *archivo = fopen(nombreArchivo, modo);
+    if (archivo == NULL) {
+        printf("Error abriendo archivo %s", nombreArchivo);
+        return 1;
+    }
+    fprintf(archivo, "%f \n", tiempo);
+    fclose(archivo);
+    puts("\nNuevo dato guardado");
+
   return 0;
 }
