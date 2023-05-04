e3fc496308e89a1cf747fc4385f40626ae89929b
MarinaLaraDionisio <131800265+MarinaLaraDionisio@users.noreply.github.com>
2023-05-04 17:38:47 +0200

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,2 +1,2 @@
 unsigned int potenciaWhile(unsigned int a, unsigned int n, int *p);
-unsigned int potenciaRecursiva(unsigned int numero,unsigned int potencia, int* p);
+unsigned int potenciaRecursiva(unsigned int numero,unsigned int potencia, int* contador);
