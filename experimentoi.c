5f88671d2e90fe0311f47a809e2994c109e4473b
Guzmanelbueno <131880631+Guzmanelbueno@users.noreply.github.com>
2023-05-05 13:24:43 +0200

    Update experimentoi.c

--- experimentoi.c
+++ experimentoi.c
@@ -4,7 +4,8 @@ hora interna del ordenador y poder operar con esas horas guardadas en nuestras v
 #include "funciones.h"
 int main(){
     unsigned int n, aux, p, memoria;/*n es la variable que usamos como base y p es el exponente al que se eleva.
-    memoria es la variable que hemos usado para medir el consumo de memoria, que estimaremos posteriormente.*/
+    memoria es la variable que hemos usado para medir el consumo de memoria, que estimaremos posteriormente. aux
+    es una variable que usaremos posteriormente en la medición del tiempo.*/
     int citer=0;/*Con esta variable podremos llevar la cuenta de cuántas veces se ha ejecutado nuestro algoritmo
     (cuántas iteraciones se han llevado a cabo). La inicializamos a 0 para evitar problemas de que pueda haber
     algo en la dirección de memoria que se le ha asignado.*/
@@ -12,7 +13,8 @@ int main(){
     double tiempo;/*Variable con la que calculamos el tiempo en milisegundos.*/
     printf("Introduzca el valor que quiere elevar: ");
     scanf("%u", &n);
-    aux=n;
+    aux=n;/*Con esto lo que hacemos es guardar el valor introducido por el usuario para poder usarlo después en la
+    medición del tiempo.*/
     /*Le pedimos al usuario que introduzca la base*/
     printf("Introduzca el valor de la potencia a la que quiere elevar: ");
     scanf("%u", &p);
@@ -31,8 +33,9 @@ int main(){
     gettimeofday(&tini, NULL);
     for(int k=0;k<10000;k++){ //Este es el bucle en el que realizamos nuestro algoritmo varias veces para medir el tiempo.
         citer=0;
-        n=aux;/*Con estos 2 pasos lo que hacemos es "resetear" nuestros operadores, evitando así que se puedan hacer números muy grandes a medida
-        que transcurre el bloque, que enlentecerían el proceso introduciendo así ruido en nuestra medición*/
+        n=aux;/*Aquí es donde entra en juego aux. Al guardar el valor introducido por el usuario podemos volver a ponerlo en n para que nuestro
+        algoritmo se ejecute siempre con los mismos parámetros. Para esto sirven estos 2 pasos. De lo contrario, cada vez tendríamos números
+        más grandes que enlentecerían todo, introduciendo errores en nuestras medidas.*/
         n=potenciaWhile(n, p, &citer);
     }
     gettimeofday(&tfin, NULL);
