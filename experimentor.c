d14880fb6108c1dd222800cdc00590f9c9fae2fb
MarinaLaraDionisio <131800265+MarinaLaraDionisio@users.noreply.github.com>
2023-05-05 13:49:56 +0200

    Update experimentor.c

--- experimentor.c
+++ experimentor.c
@@ -2,42 +2,43 @@
 #include <sys/time.h>
 #include "funciones.h"
 
+/*Este código es análogo al del experimento iterativo, por lo que todos los comentarios allí expuestos son extrapolables a este programa*/
+
 int main(){
     unsigned int n, p, crecur;
     unsigned int *const llamadas=&crecur;
-
     struct timeval tini,tfin;
     double tiempo;
-
-  printf("Introduzca el valor que quiere elevar: ");
-  scanf("%u", &n);
-
-  printf("Introduzca el valor de la potencia a la que quiere elevar: ");
-  scanf("%u", &p);
-
-  getchar();
+    
+    printf("Introduzca el valor que quiere elevar: ");
+    scanf("%u", &n);
+    
+    printf("Introduzca el valor de la potencia a la que quiere elevar: ");
+    scanf("%u", &p);
+    
+    getchar();
     gettimeofday(&tini, NULL);
-
-  for (int k=0; k<100000; k++){
-    crecur=0;
-    n=potenciaRecursiva(n, p, llamadas);
+    
+    for (int k=0; k<100000; k++){
+        crecur=0;
+        n=potenciaRecursiva(n, p, llamadas);
   }
-
-   gettimeofday(&tfin, NULL);
+    
+    gettimeofday(&tfin, NULL);
     tiempo= ((tfin.tv_sec-tini.tv_sec)*1000+(tfin.tv_usec-tini.tv_usec)/1000.0);
+    
     printf("El tiempo empleado ha sido %lf milisegundos\n", tiempo);
-
-  printf("El valor obtenido mediante recursividad es: %u\n", n);
-
-  printf("La cantidad de veces que se ha llamado a potenciaRecursiva es: %d\n", crecur);
-
-  char *nombreArchivo = "recursivo.txt";
+    printf("El valor obtenido mediante recursividad es: %u\n", n);
+    printf("La cantidad de veces que se ha llamado a potenciaRecursiva es: %d\n", crecur);
+    
+    char *nombreArchivo = "recursivo.txt";
     char *modo = "a+";// w es para sobrescribir, a+ es para añadir al existente
     FILE *archivo = fopen(nombreArchivo, modo);
     if (archivo == NULL) {
         printf("Error abriendo archivo %s", nombreArchivo);
         return 1;
     }
+    
     fprintf(archivo, "%f \n", tiempo);
     fclose(archivo);
     puts("\nNuevo dato guardado");
