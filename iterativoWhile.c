8102cca007e0ac54bb670def33e8a21936615767
Guzmanelbueno <131880631+Guzmanelbueno@users.noreply.github.com>
2023-05-05 13:55:22 +0200

    Update iterativoWhile.c

--- iterativoWhile.c
+++ iterativoWhile.c
@@ -2,8 +2,11 @@
 #include"funciones.h"
 unsigned int potenciaWhile(unsigned int n, unsigned int p, int *c)
 {
-    unsigned int resultado = n;
-    while (p > 1)
+    unsigned int resultado = n;/*creamos esta variable para poder realizar las sucesivas multiplicaciones en nuestra potencia.
+    Con esto, además, podemos hacer que haga las veces de primera iteración, ahorrándonos una dentro del bucle, siendo así
+    el proceso un poco más eficiente.*/
+    while (p > 1)/*En este bucle vamos decrementando el parámetro que pasamos como exponente hasta que llegue a 1. Con esto nos
+    aseguramos que haga el algoritmo en p-1 iteraciones.*/
     {
         resultado = resultado * n;
         *c=*c+1;
